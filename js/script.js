deeeb8953e650dc1021c84b69843c1d539638bc4
MaxAim <75140148+MaxAim@users.noreply.github.com>
2023-02-07 02:25:09 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var loaded = false;
+
 function load(){
     const percentages = document.getElementsByClassName("percentages");
     for (let element of percentages) {
@@ -6,22 +8,31 @@ function load(){
             setTimeout(() => { element.children[0].innerHTML = `${i}%`, element.children[0].style.maxWidth = element.children[0].innerHTML}, i * i / 4);
         }
     };
+    if(loaded == true) return;
     for(let i of document.getElementsByClassName("colapsable")){
         i.style.maxHeight = i.scrollHeight + "px";
     }
+    loaded = true;
 }
 
 function full(group){
     for(let i of document.getElementsByClassName(`colapsable ${group}`)){
         if(i.style.maxHeight){
             i.style.maxHeight = null;
+            i.style.marginBottom = group == "aside" ? "-20px": "-50px";
             i.style.opacity = 0;
             document.getElementsByClassName(`button ${group}`)[0].style.transform = "rotate(180deg)";
         } 
         else{
+            i.style.marginBottom = null;
             i.style.opacity = 1;
             i.style.maxHeight = i.scrollHeight + "px";
             document.getElementsByClassName(`button ${group}`)[0].style.transform = null;
+            if (group == "aside") {                
+                setTimeout(() => {
+                    load();
+                }, 0);
+            }
         } 
     }
 }
@@ -38,10 +49,6 @@ function hide(event){
         body.style.overflowY = "hidden";
         navBar.style.right = 0;
     }
-}
-
-function test(event){
-    console.log(event)
 }
 
 document.addEventListener("touchstart", hide);
